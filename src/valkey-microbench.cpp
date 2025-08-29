bea15771071985f3ebf5e89dbf161bf8bb90c2bd
Rain Valentine <rsg000@gmail.com>
2025-08-29 05:29:55 +0000

    benchmark different hit rates
    Signed-off-by: Rain Valentine <rsg000@gmail.com>

--- src/valkey-microbench.cpp
+++ src/valkey-microbench.cpp
@@ -9,33 +9,7 @@ extern "C" {
     #include "hashtable.h"
 }
 
-static void BM_HashtableFind(benchmark::State& state) {
-    // Prepare a hashtable and insert some keys
-    hashtableType type = {
-        .instant_rehashing = 1
-    };
-    hashtable *ht = hashtableCreate(&type);
-
-    const int num_keys = 16384;
-    std::vector<std::string> keys;
-    for (int i = 0; i < num_keys; ++i) {
-        keys.push_back("key" + std::to_string(i));
-        hashtableAdd(ht, (void *)keys.back().c_str());
-    }
-
-    size_t idx = 0;
-    for (auto _ : state) {
-        bool found = hashtableFind(ht, keys[idx].c_str(), nullptr);
-        benchmark::DoNotOptimize(found);
-        idx = (idx + 1) % num_keys;
-    }
-
-    hashtableRelease(ht);
-}
-
-
-
-static void BM_HashtableFind_MemClobber(benchmark::State& state) {
+static void BM_HashtableFind_0Miss(benchmark::State& state) {
     // Prepare a hashtable and insert some keys
     hashtableType type = {
         .instant_rehashing = 1
@@ -60,11 +34,79 @@ static void BM_HashtableFind_MemClobber(benchmark::State& state) {
     hashtableRelease(ht);
 }
 
-BENCHMARK(BM_HashtableFind)
+static void BM_HashtableFind_50Miss(benchmark::State& state) {
+    // Prepare a hashtable and insert some keys
+    hashtableType type = {
+        .instant_rehashing = 1
+    };
+    hashtable *ht = hashtableCreate(&type);
+
+    int num_to_remove = 16384;
+    const int num_keys = num_to_remove * 2;
+
+    std::vector<std::string> keys;
+    for (int i = 0; i < num_keys; ++i) {
+        keys.push_back("key" + std::to_string(i));
+        hashtableAdd(ht, (void *)keys.back().c_str());
+    }
+
+    for (int i = 0; i < num_keys; ++i) {
+        int chances_remaining = num_keys - i;
+        if (rand() % chances_remaining < num_to_remove) { // Probability of num_to_remove / chances_remaining
+            hashtableDelete(ht, (void *)keys.back().c_str());
+            num_to_remove--;
+        }
+    }
+
+    size_t idx = 0;
+    for (auto _ : state) {
+        bool found = hashtableFind(ht, keys[idx].c_str(), nullptr);
+        benchmark::DoNotOptimize(found);
+        idx = (idx + 1) % num_keys;
+        benchmark::ClobberMemory();
+    }
+
+    hashtableRelease(ht);
+}
+
+static void BM_HashtableFind_100Miss(benchmark::State& state) {
+    // Prepare a hashtable and insert some keys
+    hashtableType type = {
+        .instant_rehashing = 1
+    };
+    hashtable *ht = hashtableCreate(&type);
+
+    const int num_keys = 16384;
+    std::vector<std::string> keys;
+    for (int i = 0; i < num_keys; ++i) {
+        keys.push_back("key" + std::to_string(i));
+        hashtableAdd(ht, (void *)keys.back().c_str());
+    }
+
+    for (int i = 0; i < num_keys; ++i) {
+        hashtableDelete(ht, (void *)keys.back().c_str());
+    }
+
+    size_t idx = 0;
+    for (auto _ : state) {
+        bool found = hashtableFind(ht, keys[idx].c_str(), nullptr);
+        benchmark::DoNotOptimize(found);
+        idx = (idx + 1) % num_keys;
+        benchmark::ClobberMemory();
+    }
+
+    hashtableRelease(ht);
+}
+
+BENCHMARK(BM_HashtableFind_0Miss)
     ->Repetitions(5)
     ->MinTime(5.0);
 
-BENCHMARK(BM_HashtableFind_MemClobber)
+BENCHMARK(BM_HashtableFind_50Miss)
+    ->Repetitions(5)
+    ->MinTime(5.0);
+
+BENCHMARK(BM_HashtableFind_100Miss)
     ->Repetitions(5)
     ->MinTime(5.0);
 
